245ffb6fddcb01b8d166ab6a1e382ea0e1345aba
Lobinda <lobinda123@gmail.com>
2024-05-27 15:47:53 +0800

    week 5

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ const swaggerUI = require('swagger-ui-express');
 const swaggerFile = require('./swagger_output.json');
 
 const postRouter = require('./routes/posts');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const usersRouter = require('./routes/users');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
@@ -36,13 +35,13 @@ mongoose
 
 
 app.use(cors());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api-doc', swaggerUI.serve, swaggerUI.setup(swaggerFile));
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/posts', postRouter);
 
@@ -53,9 +52,9 @@ app.use(function (req, res, next) {
     message: "查無此路由，請確認API格式!",
   });
 });
-
 // 自己設定的 err 錯誤 
 const resErrorProd = (err, res) => {
+  
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: false,
@@ -80,18 +79,21 @@ const resErrorDev = (err, res) => {
 // 錯誤處理
 app.use(function(err, req, res, next) {
   // dev
+  
   err.statusCode = err.statusCode || 500;
   if (process.env.NODE_ENV === 'dev') {
     return resErrorDev(err, res);
   } 
+
   // production
-  if (err.name === 'ValidationError'){
-    console.error(err)
-    err.message = "資料欄位未填寫正確，請重新輸入！"
-    err.isOperational = true;
-    return resErrorProd(err, res)
-  }
-  resErrorProd(err, res)
+  else if (process.env.NODE_ENV === 'production') {
+        if (err.name === 'ValidationError'){
+            err.message = "欄位未填寫正確";
+            err.isOperational = true;
+            return resErrorProd(err, res)
+        }
+        resErrorProd(err, res)
+    }
 });
 
 // 未捕捉到的 catch 
